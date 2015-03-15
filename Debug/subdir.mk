e35b74d86500f5910d22d040ae0303db7598c093
Tony Darko <tony.darko@gmail.com>
2015-03-15 14:00:53 +0300

    Migrating to Eclipse CDT

---
+++ Debug/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../adc.c \
+../main.c \
+../seg_led.c \
+../spi.c \
+../uart.c 
+
+OBJS += \
+./adc.o \
+./main.o \
+./seg_led.o \
+./spi.o \
+./uart.o 
+
+C_DEPS += \
+./adc.d \
+./main.d \
+./seg_led.d \
+./spi.d \
+./uart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Compiler'
+	avr-gcc -Wall -g2 -gstabs -O0 -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega88 -DF_CPU=8000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
